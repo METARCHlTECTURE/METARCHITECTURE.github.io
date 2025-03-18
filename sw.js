0127928df8016fa81b6696b744785780322a871d
METARCHITECTURE® <1694993120@qq.com>
2025-03-19 00:37:08 +0800

    domain

--- sw.js
+++ sw.js
@@ -31,7 +31,7 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
+  "metarchltecture.github.io/METARCHITECTURE.github.io",
   "yanshuo.io",
   "cdnjs.cloudflare.com"
 ]
